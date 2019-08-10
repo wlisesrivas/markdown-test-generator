35c7966f51a3aa1a58c559419afd845c8b8cba4e
Wlises Rivas <wlisesrivas@gmail.com>
2019-08-10 09:05:23 -0400

    Javascript: questions

--- app/static/app.js
+++ app/static/app.js
@@ -1,7 +1,44 @@
-/**
- * Javascript logic
- */
 $(function(){
-   // jQuery ready.
+    $('ul.radio-list,ul.checklist,ul.textbox').each(function(i, el){
+        var questionClass = $(this).attr('class');
+        $(this).parent().addClass('question-row').addClass(questionClass);
+        if (questionClass=='checklist') {
+            $(this).find('input[type="radio"]').attr('name', 'radio-question-' + i);
+        }
+    });
+
+    function checkQuestion() {
+
+        console.log('Checking questions...');
+
+        var questions = [];
+
+        // getting single questions
+        var radio_list = $('ul.radio-list')
+        for (var i=0; i < radio_list.length; i++) {
+
+        }
+
+        showScore(3, 4);
+    }
+
+    function showScore(correct, total) {
+        var score = (correct / total).toFixed(2) * 100;
+        var msgClass = 'alert-danger';
+        if (score >= 70) {
+            msgClass = 'alert-success';
+        } else if (score >= 50) {
+            msgClass = 'alert-warning';
+        }
+        $('#tg-correct-questions').text(correct + ' de ' + total);
+        $('#tg-score').text(score);
+        $('#tg-msg').addClass(msgClass).show();
+    }
+    function resetQuestions() {
+        console.log('Reset questions...');
+        $('#tg-msg').hide();
+    }
+    $('#check-questions').on('click', checkQuestion);
+    $('#reset-questions').on('click', resetQuestions);
 
 });
